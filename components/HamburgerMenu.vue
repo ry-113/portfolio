4091599c7860a5a8981bce149c868599c0213b61
ry-113 <yamado0326@gmail.com>
2023-11-03 23:08:14 +0900

    ハンバーガーメニュー完成

--- components/HamburgerMenu.vue
+++ components/HamburgerMenu.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="hamburger inline-block md:hidden">
-    <button @click="toggleMenu">+</button>
-    <nav>
-      <ul v-if="isMenuOpen" class="mobile-menu">
-        <li>
-          <NuxtLink to="/#about" class="menu__link">About</NuxtLink>
+    <button class="z-10 text-3xl" @click="toggleMenu">
+      <font-awesome-icon :icon="['fas', 'bars']" />
+    </button>
+    <nav class="flex justify-center items-center z-20 transition ease-out duration-1000 delay-75" :class="isMenuOpen ? 'opacity-90' : 'opacity-0'">
+      <button class="absolute z-30 top-11 right-5.5 text-4xl text-white" @click="toggleMenu">
+        <font-awesome-icon :icon="['fas', 'xmark']" />
+      </button>
+      <ul class="bg-gray-500 absolute inset-0 h-screen text-white">
+        <li class="pl-5 text-xl my-3" @click="toggleMenu">
+          <NuxtLink to="/#about" class="menu__link inline-block py-2 px-4">About</NuxtLink>
         </li>
-        <li>
-          <NuxtLink to="/skills" class="menu__link">Skills</NuxtLink>
+        <li class="pl-5 text-xl my-3" @click="toggleMenu">
+          <NuxtLink to="/skills" class="menu__link inline-block py-2 px-4">Skills</NuxtLink>
         </li>
-        <li>
-          <NuxtLink to="/works" class="menu__link">Works</NuxtLink>
+        <li class="pl-5 text-xl my-3" @click="toggleMenu">
+          <NuxtLink to="/works" class="menu__link inline-block py-2 px-4">Works</NuxtLink>
         </li>
       </ul>
     </nav>
