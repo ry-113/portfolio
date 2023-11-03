0d9406c14c8c4489ba4b9ee26a0ae2f6340ff1c0
ry-113 <yamado0326@gmail.com>
2023-11-03 14:53:12 +0900

    tailwindcssモジュールの追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-microcms-module"],
+  modules: [
+    "nuxt-microcms-module",
+    "@nuxtjs/tailwindcss",
+  ],
   microCMS: {
     serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
     apiKey: process.env.MICROCMS_API_KEY,
