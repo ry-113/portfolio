f29ff264867e87b769411d11e6b7ea216be69c2d
ry-113 <yamado0326@gmail.com>
2023-11-03 23:15:53 +0900

    visibleとinvisibleを使ってハンバーガーメニューのバグを修正

--- components/HamburgerMenu.vue
+++ components/HamburgerMenu.vue
@@ -3,7 +3,7 @@
     <button class="z-10 text-3xl" @click="toggleMenu">
       <font-awesome-icon :icon="['fas', 'bars']" />
     </button>
-    <nav class="flex justify-center items-center z-20 transition ease-out duration-1000 delay-75" :class="isMenuOpen ? 'opacity-90' : 'opacity-0'">
+    <nav class="flex justify-center items-center z-20 transition ease-out duration-1000 delay-75" :class="isMenuOpen ? 'visible opacity-90' : 'invisible opacity-0'">
       <button class="absolute z-30 top-11 right-5.5 text-4xl text-white" @click="toggleMenu">
         <font-awesome-icon :icon="['fas', 'xmark']" />
       </button>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer bg-zinc-800 text-white pt-8">
+  <footer class="footer bg-zinc-800 text-white pt-8 mt-10">
     <div class="footerContainer mx-auto max-w-7xl px-5 sm:px-8 lg:px-10 flex items-center justify-between">
       <ul class="contact-address-list text-sm">
         <li>国立職業リハビリテーションセンター</li>
