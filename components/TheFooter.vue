e6a1f1a70c880e303b60f1d9a1acd1b5e6234f6c
RyoYamamoto-g <yamado0326@gmail.com>
2023-11-08 15:36:39 +0900

    フッターから職リハの情報を削除

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,9 +2,8 @@
   <footer class="glass text-white bg-zinc-700 pt-8">
     <div class="footerContainer mx-auto max-w-7xl px-5 sm:px-8 lg:px-10 flex items-center justify-between mb-6">
       <ul class="contact-address-list text-sm">
-        <li class="py-1">国立職業リハビリテーションセンター</li>
-        <li class="py-1">DTP・Web技術科</li>
-        <li class="py-1">TEL:042-995-1711</li>
+        <li class="py-1">連絡先</li>
+        <li class="py-1">DTP・Web技術科 山元 稜</li>
         <li class="py-1">E-Mail:nvdw23029@nvrcd.ac.jp</li>
       </ul>
       <ul class="sns-link-list">
