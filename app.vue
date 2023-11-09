ae53fe60c22d356dbd3804230902381ffca6e228
RyoYamamoto-g <yamado0326@gmail.com>
2023-11-09 10:58:03 +0900

    useSeoMetaでメタタグの設定とhtmlのlang属性にjaを追加

--- app.vue
+++ app.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-  useHead({
-    bodyAttrs: {
-      class: "bg-zinc-800 text-white",
-    },
-  });
+useSeoMeta({
+  charset: "utf-8",
+  title: "My Portfolio",
+  ogTitle: "My Portfolio",
+  description: "フロントエンドエンジニアを目指す職業訓練生のポートフォリオサイトです。",
+  ogDescription: "フロントエンドエンジニアを目指す職業訓練生のポートフォリオサイトです。",
+})
+useHead({
+  bodyAttrs: {
+    class: "bg-zinc-800 text-white",
+  },
+  htmlAttrs: {
+    lang: "ja",
+  }
+});
 </script>
 <template>
       <NuxtLayout>
